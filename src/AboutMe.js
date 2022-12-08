75efe36bf3b853a23bc8c0d234f19c63e18dfb04
s-fletcher <sfletchertx@gmail.com>
2022-12-07 18:42:35 -0600

    initial input

---
+++ src/AboutMe.js
@@ -0,0 +1,14 @@
+import logo from './J.png';
+import './App.css';
+
+function AboutMe() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Welcome to the About Me page!</p>
+            </header>
+        </div>
+    );
+}
+
+export default AboutMe;
